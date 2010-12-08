80d417ffc5eee84a84ee0f783ff1b85ed2361f02
Jordan Sissel <jls@semicomplete.com>
2010-12-08 01:14:40 -0800

    - Commit vnc hack. Will clean it up later.

--- fingerpoken.rb
+++ fingerpoken.rb
@@ -5,7 +5,7 @@ require "em-websocket"
 require "json"
 require "rack"
 require "sinatra/async"
-require "ffi"
+require "eventmachine-vnc"
 
 class FingerPoken < Sinatra::Base
   register Sinatra::Async
@@ -25,23 +25,37 @@ class FingerPoken < Sinatra::Base
   end # GET /style.css
 end
 
-module Xdotool
-  extend FFI::Library
-  ffi_lib "libxdo.so"
+class VNCClient < EventMachine::Connection
+  include EventMachine::Protocols::VNC::Client
 
-  attach_function :xdo_new, [:string], :pointer
-  attach_function :xdo_mousemove, [:pointer, :int, :int, :int], :int
-  attach_function :xdo_mousemove_relative, [:pointer, :int, :int], :int
-  attach_function :xdo_click, [:pointer, :long, :int], :int
-  attach_function :xdo_mousedown, [:pointer, :long, :int], :int
-  attach_function :xdo_mouseup, [:pointer, :long, :int], :int
-  attach_function :xdo_type, [:pointer, :long, :string, :long], :int
-  attach_function :xdo_keysequence, [:pointer, :long, :string, :long], :int
+  def initialize(channel)
+    @channel = channel
+
+  end
+
+  def mousemove(x, y)
+    message = [ POINTER_EVENT, 0, x, y ].pack("CCnn")
+    send_data(message)
+  end
+
+  def ready
+    puts "vnc ready"
+    @x = (@screen_width / 2).to_i
+    @y = (@screen_height / 2).to_i
+
+    @channel.subscribe do |request|
+      rel_x = request["rel_x"]
+      rel_y = request["rel_y"]
+      @x += rel_x
+      @y += rel_y
+      mousemove(@x, @y)
+    end
+  end
 end
 
 EventMachine::run do
-  xdo = Xdotool.xdo_new(nil)
-  #vnc = Net::VNC.new("sadness:0", :shared => true, :password => ENV["VNCPASS"])
+  channel = EventMachine::Channel.new
+  EventMachine::connect("sadness", 5900, VNCClient, channel)
 
   EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 5001) do |ws|
     ws.onmessage do |message|
@@ -49,40 +63,8 @@ EventMachine::run do
       p request
       case request["action"]
         when "move"
-          Xdotool.xdo_mousemove_relative(xdo, request["rel_x"], request["rel_y"])
-        when "click"
-          Xdotool.xdo_click(xdo, 0, request["button"])
-        when "mousedown"
-          Xdotool.xdo_mousedown(xdo, 0, request["button"])
-        when "mouseup"
-          Xdotool.xdo_mouseup(xdo, 0, request["button"])
-        when "type"
-          Xdotool.xdo_type(xdo, 0, request["string"], 12000)
-        when "keypress"
-          key = request["key"]
-          if key.is_a?(String)
-            if key.length == 1
-              # Assume letter
-              Xdotool.xdo_type(xdo, 0, key, 12000)
-            else
-              # Assume keysym
-              Xdotool.xdo_keysequence(xdo, 0, key, 12000)
-            end
-          else
-            # type printables, key others.
-            if 32.upto(127).include?(key)
-              Xdotool.xdo_type(xdo, 0, request["key"].chr, 12000)
-            else
-              case key
-                when 8 
-                  Xdotool.xdo_keysequence(xdo, 0, "BackSpace", 12000)
-                when 13
-                  Xdotool.xdo_keysequence(xdo, 0, "Return", 12000)
-                else
-                  puts "I don't know how to type web keycode '#{key}'"
-                end # case key
-            end # if 32.upto(127).include?(key)
-          end # if key.is_a?String
+          #Xdotool.xdo_mousemove_relative(xdo, request["rel_x"], request["rel_y"])
+          channel.push(request)
       end # case request["action"]
     end # ws.onmessage
   end # WebSocket
